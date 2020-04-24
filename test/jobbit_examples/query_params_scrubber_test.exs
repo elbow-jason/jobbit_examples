cb24985ea8ffc76b1d42be4c3664b0bb2fc40d74
Jason Goldberger <jlgoldb2@asu.edu>
2020-04-24 15:16:20 -0700

    finished QueryParamsScrubber tests

--- test/jobbit_examples/query_params_scrubber_test.exs
+++ test/jobbit_examples/query_params_scrubber_test.exs
@@ -4,6 +4,7 @@ defmodule Jobbit.QueryParamsScrubberTest do
   alias JobbitExamples.QueryParamsScrubber.Declaratively
   alias JobbitExamples.QueryParamsScrubber.Defensively
   alias JobbitExamples.QueryParamsScrubber.WithJobbit
+  alias Jobbit.TaskError
 
   @good_string "?jason=1&adam=2&mike=1&zach=2&xn=1"
   @bad_value_in_a_pair @good_string <> "&other=not_one_nor_two"
@@ -42,6 +43,46 @@ defmodule Jobbit.QueryParamsScrubberTest do
 
     test "crashes for non-string" do
       assert_raise(FunctionClauseError, fn -> Declaratively.parse(@non_string) end)
+    end
+  end
+
+  describe "Defensively" do
+    test "works for valid rule-matching query params string" do
+      assert Defensively.parse(@good_string) == {:ok, @success_list}
+    end
+
+    test "errors for bad value in one of the pairs" do
+      assert :error = Defensively.parse(@bad_value_in_a_pair)
+    end
+
+    test "errors for invalid formats" do
+      Enum.each(@bad_formats, fn bad_format ->
+        assert :error = Defensively.parse(bad_format)
+      end)
+    end
+
+    test "errors for non-string" do
+      assert :error = Defensively.parse(@non_string)
+    end
+  end
+
+  describe "WithJobbit" do
+    test "works for valid rule-matching query params string" do
+      assert WithJobbit.parse(@good_string) == {:ok, @success_list}
+    end
+
+    test "errors for bad value in one of the pairs" do
+      assert {:error, %TaskError{}} = WithJobbit.parse(@bad_value_in_a_pair)
+    end
+
+    test "errors for invalid formats" do
+      Enum.each(@bad_formats, fn bad_format ->
+        assert {:error, %TaskError{}} = WithJobbit.parse(bad_format)
+      end)
+    end
+
+    test "errors for non-string" do
+      assert {:error, %TaskError{}} = WithJobbit.parse(@non_string)
     end
   end
 end
